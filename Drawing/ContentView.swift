c5e46769a1128a4c45b6f4436553f9a2a55748e8
Tsungyu Yu <ytyubox@gmail.com>
2020-03-19 11:57:23 +0800

    Initial Commit

---
+++ Drawing/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Drawing
+//
+//  Created by 游宗諭 on 2020/3/19.
+//  Copyright © 2020 游宗諭. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
